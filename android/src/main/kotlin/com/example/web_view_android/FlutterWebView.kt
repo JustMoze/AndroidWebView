0da3e77f05df0c6812b7823b39fe26ce05d1803f
Modestas Rimeikis <modestas.rimeikis@telesoftas.com>
2024-01-14 10:56:47 +0200

    Update android webview to handle downloads in android

--- android/src/main/kotlin/com/example/web_view_android/FlutterWebView.kt
+++ android/src/main/kotlin/com/example/web_view_android/FlutterWebView.kt
@@ -1,17 +1,23 @@
 package com.example.web_view_android
 
+import android.annotation.SuppressLint
+import android.app.DownloadManager
 import android.content.Context
+import android.net.Uri
+import android.os.Environment
 import android.view.View
+import android.webkit.CookieManager
+import android.webkit.URLUtil
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.platform.PlatformView
-import android.webkit.CookieManager
 
-
+@SuppressLint("SetJavaScriptEnabled")
 class FlutterWebView internal constructor(
     context: Context,
     messenger: BinaryMessenger,
@@ -20,29 +26,45 @@ class FlutterWebView internal constructor(
     PlatformView, MethodCallHandler {
     private val webView: WebView
     private val methodChannel: MethodChannel
+
     override fun getView(): View {
         return webView
     }
 
     init {
-        // Init WebView
         webView = WebView(context)
         webView.settings.javaScriptEnabled = true
-        webView.webViewClient = WebViewClient()
+        val customWebViewClient = CustomWebViewClient()
+        webView.webViewClient = customWebViewClient
+        webView.setDownloadListener { url, userAgent, contentDisposition, mimeType, contentLength ->
+            val request = DownloadManager.Request(Uri.parse(url))
+            request.setMimeType(mimeType)
+            request.addRequestHeader("cookie", CookieManager.getInstance().getCookie(url))
+            request.addRequestHeader("User-Agent", userAgent)
+            request.setDescription("Downloading file...")
+            request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimeType))
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            request.setDestinationInExternalFilesDir(
+                context,
+                Environment.DIRECTORY_DOWNLOADS,
+                ".png"
+            )
+            val dm = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+            dm.enqueue(request)
+            Toast.makeText(context, "Downloading File", Toast.LENGTH_LONG).show()
+        }
         methodChannel = MethodChannel(messenger, "plugins.example/flutter_web_view_$id")
-        // Init methodCall Listener
         methodChannel.setMethodCallHandler(this)
     }
 
     override fun onMethodCall(methodCall: MethodCall, result: MethodChannel.Result) {
         when (methodCall.method) {
-            "setUrl" -> setText(methodCall, result)
+            "setUrl" -> setUrl(methodCall, result)
             else -> result.notImplemented()
         }
     }
 
-    // set and load new Url
-    private fun setText(methodCall: MethodCall, result: MethodChannel.Result ) {
+    private fun setUrl(methodCall: MethodCall, result: MethodChannel.Result) {
         val url = methodCall.arguments as String
         val cookieString = "is_app=1; path=/; secure; HttpOnly"
         CookieManager.getInstance().setCookie(url, cookieString)
@@ -50,9 +72,15 @@ class FlutterWebView internal constructor(
         result.success(null)
     }
 
-    // Destroy WebView when PlatformView is destroyed
     override fun dispose() {
         webView.destroy()
     }
 
+    private inner class CustomWebViewClient : WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView, Url: String): Boolean {
+            view.loadUrl(Url)
+            return true
+
+        }
+    }
 }
